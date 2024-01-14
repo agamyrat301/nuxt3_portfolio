a6890bcdc25d05dc408a0cd5bf41f3af8b5c9d9c
agamyrat301 <agajan301@gmail.com>
2024-01-14 22:00:34 +0200

    meta tags added

--- app.vue
+++ app.vue
@@ -70,5 +70,28 @@ const confirmDownload = () => {
   alert('Not this time!!!')
 }
 
+
+useHead({
+  htmlAttrs: { lang: 'en' },
+  title: 'Agamyrat Akmyradov',
+  meta: [
+    {
+      name: "title",
+      content: 'Agamyrat Akmyradov',
+    },
+    {
+      name: "description",
+      content: 'passionate web developer',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/icon.ico',
+    },
+  ],
+});
+
 const title = ref('<Agamyrat/>')
 </script>
